043d30292626c432af2f22c59f007b7f81e19e20
Kaku Matsu <kaku@matsu.dev>
2021-08-22 15:14:17 +0800

    [gui] Add filter

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/gotk3/gotk3/glib"
@@ -34,6 +35,67 @@ func mustGetObj(builder *gtk.Builder, id string) glib.IObject {
 		logrus.Panicf("cannot get %s: %v", id, err)
 	}
 	return obj
+}
+
+func setupInputEvents(builder *gtk.Builder) {
+	capitalizeInputHandler := func(editable *gtk.Entry) {
+		buf := editable.GetChars(0, -1)
+		capBuf2 := strings.Builder{}
+		filtered := false
+		for _, r := range buf {
+			if (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9') || (r == '.') || (r == '/') || (r == '_') {
+				capBuf2.WriteRune(r)
+			} else if r >= 'a' && r <= 'z' {
+				capBuf2.WriteRune(r - 'a' + 'A')
+				filtered = true
+			} else {
+				filtered = true
+			}
+		}
+		if !filtered {
+			return
+		}
+		editableBuf, err := editable.GetBuffer()
+		if err != nil {
+			return
+		}
+		editableBuf.SetText(capBuf2.String())
+	}
+	numericInputHandler := func(editable *gtk.Entry) {
+		buf := editable.GetChars(0, -1)
+		capBuf2 := strings.Builder{}
+		filtered := false
+		for _, r := range buf {
+			if r >= '0' && r <= '9' {
+				capBuf2.WriteRune(r)
+			} else {
+				filtered = true
+			}
+		}
+		if !filtered {
+			return
+		}
+		editableBuf, err := editable.GetBuffer()
+		if err != nil {
+			return
+		}
+		editableBuf.SetText(capBuf2.String())
+	}
+
+	inputCallsign := mustGetObj(builder, "input-callsign").(*gtk.Entry)
+	inputCallsign.ConnectAfter("changed", capitalizeInputHandler)
+
+	inputExchSent := mustGetObj(builder, "input-exch-sent").(*gtk.Entry)
+	inputExchSent.ConnectAfter("changed", capitalizeInputHandler)
+
+	inputExchRcvd := mustGetObj(builder, "input-exch-rcvd").(*gtk.Entry)
+	inputExchRcvd.ConnectAfter("changed", capitalizeInputHandler)
+
+	inputRstSent := mustGetObj(builder, "input-rst-sent").(*gtk.Entry)
+	inputRstSent.ConnectAfter("changed", numericInputHandler)
+
+	inputRstRcvd := mustGetObj(builder, "input-rst-rcvd").(*gtk.Entry)
+	inputRstRcvd.ConnectAfter("changed", numericInputHandler)
 }
 
 func main() {
@@ -87,6 +149,8 @@ func main() {
 	utcClockLabel.SetLabel(fmt.Sprintf("%02d:%02d:%02dZ", current.Hour(), current.Minute(), current.Second()))
 
 	go timer(appContextWithCancel, utcClockLabel)
+
+	setupInputEvents(builder)
 
 	gtk.Main()
 }
